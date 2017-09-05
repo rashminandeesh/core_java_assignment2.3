e5a2871066eca255e23400691eef88b9e9c84449
Latitude <Latitude@192.168.1.5>
2017-09-05 21:13:32 +0530

    third commit

---
+++ src/andOperation.java
@@ -0,0 +1,14 @@
+
+public class andOperation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a,b,c;
+a=10;
+b=25;
+c=a&b;
+System.out.println("a&b="+c);
+	}
+
+}
+
